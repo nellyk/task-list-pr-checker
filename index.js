f15fb2368a4091360b0054f48d586506290a9d18
nkiboi <nelly.j.kiboi@aexp.com>
2020-03-09 13:36:36 +0000

    testing actions

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const github = require('@actions/github');
 const core = require('@actions/core');
+const webhooks = require('@octokit/webhooks');
 
 const myToken = core.getInput('myToken');
 
-//const octokit = new github.GitHub(myToken);
+const octokit = new github.GitHub(myToken);
 const context = github.context;
-console.log(context);
-//console.log(octokit);
+
+const newIssue = await octokit.issues.create({
+  ...context.repo,
+  title: 'New issue!',
+  body: 'Hello Universe!'
+});
+
+newIssue();
