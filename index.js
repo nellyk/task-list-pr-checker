25738331e2b136b81bb7f2125513173932ee536d
nkiboi <nelly.j.kiboi@aexp.com>
2020-03-09 13:26:22 +0000

    test

--- index.js
+++ index.js
@@ -2,10 +2,15 @@ const github = require('@actions/github');
 const core = require('@actions/core');
 const webhooks = require('@octokit/webhooks');
 
+const myToken = core.getInput('myToken');
+
+const octokit = new github.GitHub(myToken);
 const context = github.context;
 
-if (context.eventName === 'push') {
-    const pushPayload = webhooks.WebhookPayloadPush;
-    core.info(`The head commit is: ${pushPayload.head}`)
-    console.log(pushPayload);
-  }
+const github = require('@actions/github');
+
+const newIssue = await octokit.issues.create({
+  ...context.repo,
+  title: 'New issue!',
+  body: 'Hello Universe!'
+});
