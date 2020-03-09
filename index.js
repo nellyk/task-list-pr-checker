449859688a4e7dfdec5182c20095912d1c86bfbe
nkiboi <nelly.j.kiboi@aexp.com>
2020-03-09 13:30:34 +0000

    test

--- index.js
+++ index.js
@@ -6,7 +6,5 @@ const myToken = core.getInput('myToken');
 
 const octokit = new github.GitHub(myToken);
 const context = github.context;
-
-const github = require('@actions/github');
-
 console.log(context);
+console.log(octokit);
