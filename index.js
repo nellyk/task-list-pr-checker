0b03f63e2c3b9911c8c7afcb5386392d51d85851
nkiboi <nelly.j.kiboi@aexp.com>
2020-03-09 13:22:05 +0000

    test

--- index.js
+++ index.js
@@ -7,27 +7,5 @@ const context = github.context;
 if (context.eventName === 'push') {
     const pushPayload = webhooks.WebhookPayloadPush;
     core.info(`The head commit is: ${pushPayload}`)
+    console.log(pushPayload);
   }
-
-  async function run() {
-    // This should be a token with access to your repository scoped in as a secret.
-    // The YML workflow will need to set myToken with the GitHub Secret Token
-    // myToken: ${{ secrets.GITHUB_TOKEN }}
-    // https://help.github.com/en/actions/automating-your-workflow-with-github-actions/authenticating-with-the-github_token#about-the-github_token-secret
-    const myToken = core.getInput('myToken');
-
-    const octokit = new github.GitHub(myToken);
-
-    const { data: pullRequest } = await octokit.pulls.get({
-        owner: 'octokit',
-        repo: 'rest.js',
-        pull_number: 3,
-        mediaType: {
-          format: 'diff'
-        }
-    });
-
-    console.log(pullRequest);
-}
-
-run()
