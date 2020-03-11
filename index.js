3cc507f12aa684ebb183d452b45f7b7ca1e79a11
nkiboi <nelly.j.kiboi@aexp.com>
2020-03-11 14:37:13 +0000

    test

--- index.js
+++ index.js
@@ -1,53 +1,50 @@
 const github = require('@actions/github');
 const core = require('@actions/core');
-const webhooks = require('@octokit/webhooks');
 
 async function run() {
+  try {
+    const githubToken = core.getInput('GITHUB_TOKEN');
 
-    try {
-const github_token = core.getInput('GITHUB_TOKEN');
 
+    const octokit = new github.GitHub(githubToken);
+    const { context } = github;
 
-const octokit = new github.GitHub(github_token);
-const context = github.context;
+    if (context.payload.pull_request == null) {
+      core.setFailed('No pull request found.');
+      return;
+    }
+    const body = context.eventName === ('issue_comment' || 'pull_request_review_comment') ? context.payload.comment.body : context.payload.pull_request.body;
+    const pullRequestNumber = context.payload.pull_request.number;
+    const isUnChecked = /-\s\[\s\]/g.test(body);
+    const status = isUnChecked ? 'pending' : 'success';
 
-if (context.payload.pull_request == null) {
-    core.setFailed('No pull request found.');
-    return;
-}
-const pull_request_number = context.payload.pull_request.number;
-
-const title = context.payload.pull_request.title;
-const body = context.eventName === ("issue_comment" || "pull_request_review_comment") ? context.payload.comment.body: context.payload.pull_request.body;
-
-const isUnChecked = /-\s\[\s\]/g.test(body);
-const status = isUnChecked ? "pending" : "success";
-const message = `Updating PR "${title}" (${context.payload.pull_request
-    .html_url}): ${status}`;
-
-  const checkStatus =  octokit.repos.createStatus({
-      ...context.repo,  
+    const checkStatus = octokit.repos.createStatus({
+      ...context.repo,
       sha: context.payload.pull_request.head.sha,
       state: status,
-      description: status === "pending" ? "Pending tasks" : "Done tasks",
-      context: "tasks"
-  });
-  core.setOutput('comment_body', body);
+      description: status === 'pending' ? 'Pending tasks' : 'Done tasks',
+      context: 'tasks',
+    });
+
+    const issueComment = octokit.repos.issue_comments({
+      ...context.repo,
+      number: pullRequestNumber,
+    });
+    core.setOutput('issueComment', issueComment);
 
 
-console.log(context)
-console.log(context.payload);
-console.log(`muhahaha`);
-console.log(context.payload.pull_request);
-console.log(`muhahaha`);
-console.log(context.payload.pull_request.comments);
-console.log(`muhahaha`);
-console.log(context.payload.comment);
-console.log(`muhahaha`);
-console.log(checkStatus);
-    }catch(error){
+    console.log(context);
+    console.log(context.payload);
+    console.log('muhahaha');
+    console.log(context.payload.pull_request);
+    console.log('muhahaha');
+    console.log(context.payload.pull_request.comments);
+    console.log('muhahaha');
+    console.log(context.payload.comment);
+    console.log('muhahaha');
+    console.log(checkStatus);
+  } catch (error) {
     core.setFailed(error.message);
-    }
-
+  }
 }
 run();
