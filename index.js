dfdc163ba5e9c952b46294b69bcfed666d081899
nkiboi <nelly.j.kiboi@aexp.com>
2020-03-12 10:56:34 +0000

    testing

--- index.js
+++ index.js
@@ -28,30 +28,34 @@ async function run() {
         issueComment = issueComments[index];
         if (context.eventName === 'pull_request_review_comment') {
           body = context.payload.comment.body;
+        } else if (issueComment.body !== null) {
+          body = issueComment.body;
         } else {
           body = context.payload.pull_request.body;
         }
         console.log(issueComment);
       }
     } else {
-      // check if undefined then get the spefic comment
-      // pull request number afterwards pass this down tho the method
-      const { data: listComments } = await octokit.pulls.listComments({
-        ...context.repo,
-        pull_number: context.payload.issue.number,
-      });
-
-      for (let index = 0; index < listComments.length; index += 1) {
-        const comment = listComments[index];
-        body = comment.body;
-        console.log(`This is the comment yebo ${body}`);
-      }
+      console.log(context.payload);
+      body = context.payload.comment.body;
     }
     const isUnChecked = /-\s\[\s\]/g.test(body);
     const status = isUnChecked ? 'pending' : 'success';
-    const checkStatus = octokit.repos.createStatus({
+    let sha;
+    // check if undefined then get the spefic comment
+    // pull request number afterwards pass this down tho the method
+    if (context.payload.comment !== undefined) {
+      const { data: pullRequest } = await octokit.pulls.get({
+        ...context.repo,
+        pull_number: context.payload.issue.number,
+      });
+      sha = pullRequest.head.sha;
+      console.log(`yebo ${sha}`);
+    }
+    sha = context.eventName !== 'issue_comment' ? context.payload.pull_request.head.sha : sha;
+    const checkStatus = octokit.pulls.get({
       ...context.repo,
-      sha: context.payload.pull_request.head.sha,
+      sha,
       state: status,
       description: status === 'pending' ? 'Pending tasks' : 'Done tasks',
       context: 'tasks',
