53fa1decf3850cad3bdddc5148ace900a3051da4
nkiboi <nelly.j.kiboi@aexp.com>
2020-03-12 13:14:31 +0000

    testing

--- index.js
+++ index.js
@@ -4,26 +4,20 @@ const core = require('@actions/core');
 async function run() {
   try {
     const githubToken = core.getInput('GITHUB_TOKEN');
-
-
     const octokit = new github.GitHub(githubToken);
     const { context } = github;
     let body;
     let issueComment;
     if (context.eventName === 'pull_request') {
       const pullRequestNumber = context.payload.pull_request.number;
-      if (context.payload.pull_request == null) {
+      if (context.payload.pull_request === null) {
         core.setFailed('No pull request found.');
         return;
       }
-
       const { data: issueComments } = await octokit.issues.listComments({
         ...context.repo,
         issue_number: pullRequestNumber,
       });
-
-
-      console.log(issueComments);
       for (let index = 0; index < issueComments.length; index += 1) {
         issueComment = issueComments[index];
         if (issueComment.body !== null) {
@@ -31,10 +25,8 @@ async function run() {
         } else {
           body = context.payload.pull_request.body;
         }
-        console.log(issueComment);
       }
     } else {
-      console.log(context.payload);
       body = context.payload.comment.body;
     }
     const isUnChecked = /-\s\[\s\]/g.test(body);
@@ -46,24 +38,15 @@ async function run() {
         pull_number: context.payload.issue.number,
       });
       sha = pullRequest.head.sha;
-      console.log(`yebo ${sha}`);
     }
     sha = context.eventName !== 'issue_comment' ? context.payload.pull_request.head.sha : sha;
-    const checkStatus = octokit.repos.createStatus({
+    const { data: checkStatus } = await octokit.repos.createStatus({
       ...context.repo,
       sha,
       state: status,
       description: status === 'pending' ? 'Pending tasks' : 'Done tasks',
       context: 'tasks',
     });
-    console.log(context);
-    console.log(context.payload);
-    console.log('muhahaha');
-    console.log(context.payload.pull_request);
-    console.log('muhahaha');
-    console.log('muhahaha');
-    console.log(context.payload.comment);
-    console.log('muhahaha');
     console.log(checkStatus);
   } catch (error) {
     core.setFailed(error.message);
